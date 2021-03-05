a8ae6aac6bc524e10ac716473f1f5a299d6d65b5
Hemil <hemil.desai10@gmail.com>
2021-03-05 12:23:10 +0000

    Change norm to work with fp16 and non distributed training

--- mmdet3d/ops/norm.py
+++ mmdet3d/ops/norm.py
@@ -7,12 +7,9 @@ from torch.autograd.function import Function
 
 
 class AllReduce(Function):
-
     @staticmethod
     def forward(ctx, input):
-        input_list = [
-            torch.zeros_like(input) for k in range(dist.get_world_size())
-        ]
+        input_list = [torch.zeros_like(input) for k in range(dist.get_world_size())]
         # Use allgather instead of allreduce in-place operations is unreliable
         dist.all_gather(input_list, input, async_op=False)
         inputs = torch.stack(input_list, dim=0)
@@ -24,7 +21,7 @@ class AllReduce(Function):
         return grad_output
 
 
-@NORM_LAYERS.register_module('naiveSyncBN1d')
+@NORM_LAYERS.register_module("naiveSyncBN1d")
 class NaiveSyncBatchNorm1d(nn.BatchNorm1d):
     """Syncronized Batch Normalization for 3D Tensors.
 
@@ -43,20 +40,21 @@ class NaiveSyncBatchNorm1d(nn.BatchNorm1d):
         It is slower than `nn.SyncBatchNorm`.
     """
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *args, fp16_enabled=False, **kwargs):
         super().__init__(*args, **kwargs)
-        self.fp16_enabled = False
+        self.fp16_enabled = fp16_enabled
 
     # customized normalization layer still needs this decorator
     # to force the input to be fp32 and the output to be fp16
     # TODO: make mmcv fp16 utils handle customized norm layers
-    @force_fp32(out_fp16=True)
+    # @force_fp32(out_fp16=True)
     def forward(self, input):
-        assert input.dtype == torch.float32, \
-            f'input should be in float32 type, got {input.dtype}'
-        if dist.get_world_size() == 1 or not self.training:
+        # assert (
+        #     input.dtype == torch.float32
+        # ), f"input should be in float32 type, got {input.dtype}"
+        if not self.training or not dist.is_initialized() or dist.get_world_size() == 1:
             return super().forward(input)
-        assert input.shape[0] > 0, 'SyncBN does not support empty inputs'
+        assert input.shape[0] > 0, "SyncBN does not support empty inputs"
         C = input.shape[1]
         mean = torch.mean(input, dim=[0, 2])
         meansqr = torch.mean(input * input, dim=[0, 2])
@@ -66,8 +64,7 @@ class NaiveSyncBatchNorm1d(nn.BatchNorm1d):
 
         mean, meansqr = torch.split(vec, C)
         var = meansqr - mean * mean
-        self.running_mean += self.momentum * (
-            mean.detach() - self.running_mean)
+        self.running_mean += self.momentum * (mean.detach() - self.running_mean)
         self.running_var += self.momentum * (var.detach() - self.running_var)
 
         invstd = torch.rsqrt(var + self.eps)
@@ -78,7 +75,7 @@ class NaiveSyncBatchNorm1d(nn.BatchNorm1d):
         return input * scale + bias
 
 
-@NORM_LAYERS.register_module('naiveSyncBN2d')
+@NORM_LAYERS.register_module("naiveSyncBN2d")
 class NaiveSyncBatchNorm2d(nn.BatchNorm2d):
     """Syncronized Batch Normalization for 4D Tensors.
 
@@ -97,21 +94,22 @@ class NaiveSyncBatchNorm2d(nn.BatchNorm2d):
         It is slower than `nn.SyncBatchNorm`.
     """
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *args, fp16_enabled=False, **kwargs):
         super().__init__(*args, **kwargs)
-        self.fp16_enabled = False
+        self.fp16_enabled = fp16_enabled
 
     # customized normalization layer still needs this decorator
     # to force the input to be fp32 and the output to be fp16
     # TODO: make mmcv fp16 utils handle customized norm layers
-    @force_fp32(out_fp16=True)
+    # @force_fp32(out_fp16=True)
     def forward(self, input):
-        assert input.dtype == torch.float32, \
-            f'input should be in float32 type, got {input.dtype}'
-        if dist.get_world_size() == 1 or not self.training:
+        # assert (
+        #     input.dtype == torch.float32
+        # ), f"input should be in float32 type, got {input.dtype}"
+        if not self.training or not dist.is_initialized() or dist.get_world_size() == 1:
             return super().forward(input)
 
-        assert input.shape[0] > 0, 'SyncBN does not support empty inputs'
+        assert input.shape[0] > 0, "SyncBN does not support empty inputs"
         C = input.shape[1]
         mean = torch.mean(input, dim=[0, 2, 3])
         meansqr = torch.mean(input * input, dim=[0, 2, 3])
@@ -121,8 +119,7 @@ class NaiveSyncBatchNorm2d(nn.BatchNorm2d):
 
         mean, meansqr = torch.split(vec, C)
         var = meansqr - mean * mean
-        self.running_mean += self.momentum * (
-            mean.detach() - self.running_mean)
+        self.running_mean += self.momentum * (mean.detach() - self.running_mean)
         self.running_var += self.momentum * (var.detach() - self.running_var)
 
         invstd = torch.rsqrt(var + self.eps)
