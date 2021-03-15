3b561526330e785dfbcb52c95909e7086864ceb0
Hemil <hemil.desai10@gmail.com>
2021-03-15 15:01:44 +0000

    Cleanup ds

--- ds/optimizer.py
+++
@@ -1,7 +0,0 @@
-from mmcv.runner.hooks import HOOKS, Hook
-
-@HOOKS.register_module()
-class DSOptimizerHook(Hook):
-    def after_train_iter(self, runner):
-        runner.model.backward(runner.outputs['loss'])
-        runner.model.step()
